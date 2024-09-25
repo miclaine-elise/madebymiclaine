f1a55125393aad0525e734ea8a877c672006b047
Miclaine Emtman <clainie@gmail.com>
2024-09-25 12:21:08 -0700

    Switch to hash router

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import routes from './routes';
-import './App.css'
-const router = createBrowserRouter(routes);
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import './App.css';
+import App from './App';
+import About from './About';
+import Software from './Software';
+import Privacy from './Privacy';
+import Art from './Art';
 
+// Define routes directly inside the HashRouter component
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="/" element={<Navigate to="/about" />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/software" element={<Software />} />
+          <Route path="/art" element={<Art />} />
+          <Route path="/privacy" element={<Privacy />} />
+        </Route>
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
+
